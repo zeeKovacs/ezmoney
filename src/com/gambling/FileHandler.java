588cb3b5bf401f8e65858cb2f15193a3f0c4bdd7
Berta Vincze <berta.vincze@gmail.com>
2019-01-14 11:46:48 +0100

    new filehandler

--- src/com/gambling/FileHandler.java
+++ src/com/gambling/FileHandler.java
@@ -9,12 +9,18 @@ import java.io.FileWriter;
 
 public class FileHandler {
 
-    public String[] Read(String file) {
-        String[] data = new String[fileLines(file)];
+    public String[] Read(String file) { 
 
         try (BufferedReader br = new BufferedReader(new FileReader(file))) {
             while (br.ready()) {
-                data = br.readLine().split(",");              
+                int numOfLines = fileLines(file);
+                String countFieldString = br.readLine();
+                int numOfFields = countFieldString.length() - countFieldString.replace(",", "").length() + 1;
+                
+                String[][] data = new String[numOfLines][numOfFields];
+                for (int i = 0; i < numOfLines; i++) {
+                    data[i] = br.readLine().split(",");
+                }
             }
             return data;
 
