8612afca707496fb9d47acdd2c35f6b96a8c1eab
Zoltan <zolee95@gmail.com>
2019-01-14 11:04:06 +0100

    fh

--- src/com/gambling/FileHandler.java
+++ src/com/gambling/FileHandler.java
@@ -10,7 +10,7 @@ import java.io.FileWriter;
 public class FileHandler {
 
     public String[] Read(String file) {
-        String[] data = new String[8];
+        String[] data = new String[fileLines(file)];
 
         try (BufferedReader br = new BufferedReader(new FileReader(file))) {
             while (br.ready()) {
@@ -36,6 +36,23 @@ public class FileHandler {
             }
         } catch (IOException e) {
             System.out.println("IO Exception occured!");
+        }
+    }
+
+    public int fileLines(String file) {
+        int i = 0;
+        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+            while (br.ready()) {
+                br.readLine();
+                i++;
+            }
+            return i;
+        } catch (FileNotFoundException f) {
+            System.out.println("File not found!");
+            return 0;
+        } catch (IOException e) {
+            System.out.println("IO Exception occured!");
+            return 0;
         }
     }
 }
