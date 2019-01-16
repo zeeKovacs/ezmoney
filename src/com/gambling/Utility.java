83dbe2184369cb8ba42503658476e1aad3e77ddb
szilardGaal <gaalszilard23@gmail.com>
2019-01-16 11:53:40 +0100

    test this!

--- src/com/gambling/Utility.java
+++ src/com/gambling/Utility.java
@@ -1,21 +1,25 @@
 package com.gambling;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
 import java.util.Random;
 
 public class Utility {
 
-    public static T[] addToArray(T[] list, T toAdd) {
-        T[] tempList = (T[])new Object[list.length+1];
+    public static T[] addToArray(T[] list, T toAdd, Class<T> type) {
+        @SuppressWarnings("unchecked")
+        T[] tempList = (T[]) Array.newInstance(type, list.length-1);
         System.arraycopy(list, 0, tempList, 0, tempList.length-1);
         tempList[list.length] = toAdd;
         list = tempList;
         return list;
     }
 
-    public static <T> T[] removeFromArray(T[] list, T toRemove) {
+    public static <T> T[] removeFromArray(T[] list, T toRemove, Class<T> type) {
         for (int i=0; i < list.length; i++) {
             if (list[i].equals(toRemove)) {
-                T tempList[] = (T[])new Object[list.length-1];
+                @SuppressWarnings("unchecked")
+                T[] tempList = (T[]) Array.newInstance(type, list.length-1);
                 System.arraycopy(list, 0, tempList, 0, i);
                 System.arraycopy(list, i+1, tempList, i, list.length-i-1);
                 return tempList;
