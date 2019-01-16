cab299ee06264fd45824dcda6c1b9608980bd73d
szilardGaal <gaalszilard23@gmail.com>
2019-01-16 10:05:28 +0100

    utility

--- src/com/gambling/Utility.java
+++ src/com/gambling/Utility.java
@@ -4,38 +4,18 @@ import java.util.Random;
 
 public class Utility {
 
-    public static Horse[] addToArray(Horse[] list, Horse toAdd) {
-        Horse[] tempList = new Horse[list.length+1];
+    public static T[] addToArray(T[] list, T toAdd) {
+        T[] tempList = (T[])new Object[list.length+1];
         System.arraycopy(list, 0, tempList, 0, tempList.length-1);
         tempList[list.length] = toAdd;
         list = tempList;
         return list;
     }
 
-    public static String[] addToArray(String[] list, String toAdd) {
-        String[] tempList = new String[list.length+1];
-        System.arraycopy(list, 0, tempList, 0, tempList.length-1);
-        tempList[list.length] = toAdd;
-        list = tempList;
-        return list;
-    }
-
-    public static Horse[] removeFromArray(Horse[] list, Horse toRemove) {
+    public static <T> T[] removeFromArray(T[] list, T toRemove) {
         for (int i=0; i < list.length; i++) {
             if (list[i].equals(toRemove)) {
-                Horse tempList[] = new Horse[list.length-1];
-                System.arraycopy(list, 0, tempList, 0, i);
-                System.arraycopy(list, i+1, tempList, i, list.length-i-1);
-                return tempList;
-            }
-        }
-        return list;
-    }
-
-    public static String[] removeFromArray(String[] list, String toRemove) {
-        for (int i=0; i < list.length; i++) {
-            if (list[i].equals(toRemove)) {
-                String tempList[] = new String[list.length-1];
+                T tempList[] = (T[])new Object[list.length-1];
                 System.arraycopy(list, 0, tempList, 0, i);
                 System.arraycopy(list, i+1, tempList, i, list.length-i-1);
                 return tempList;
